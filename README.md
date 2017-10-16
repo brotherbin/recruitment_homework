ffe79affb65a114828daad4138a2cb1d6d3d9e2f
brotherbin <brotherbin@outlook.com>
2017-10-17 00:37:06 +0800

    finished the work

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # recruitment_homework
 ThoughtWorks_campus_recruitment_homework
+
+##Note:
+assume the last time of maintain is the purchasing time, otherwise, we can't get the notify time 
+
+## 1. install python enviroment
+learn more at https://wiki.python.org/moin/BeginnersGuide/Download
+
+## 2. execute python file
+> cd ${PROJECT_DIR}
+> python ThoughtWorks_campus_recruitment_homework.py
--- ThoughtWorks_campus_recruitment_homework.py
+++ ThoughtWorks_campus_recruitment_homework.py
@@ -3,7 +3,7 @@
 
 
 import time
-from datetime import datetime
+from datetime import datetime,timedelta,date
 
 NO_MAINTENANCE = 0
 TIME_RELATED_MAINTENANCE = 1
@@ -12,23 +12,31 @@ WRITE_OFF_MAINTENANCE = 3
 
 def main():
 	submit_date, cars = get_input()
-	no_maintenance_cars = []
-	write_off_cars = []
-	distance_related_maintenance_cars = []
-	time_related_maintenance_cars = []
+	write_off_cars = dict()
+	distance_related_maintenance_cars = dict()
+	time_related_maintenance_cars = dict()
 	for car in cars:
 		notify_type = car.notify_maintenance(submit_date)
 		if notify_type == NO_MAINTENANCE:
 			continue
 		elif notify_type == WRITE_OFF_MAINTENANCE:
-			write_off_cars.append(car)
+			if write_off_cars.has_key(car.brand_name):
+				write_off_cars[car.brand_name].append(car)
+			else:
+				write_off_cars[car.brand_name] = [car]
 		elif notify_type == DISTANCE_RELATED_MAINTENANCE:
-			distance_related_maintenance_cars.append(car)
+			if  distance_related_maintenance_cars.has_key(car.brand_name):
+				distance_related_maintenance_cars[car.brand_name].append(car)
+			else:
+				distance_related_maintenance_cars[car.brand_name] = [car]
 		elif notify_type == TIME_RELATED_MAINTENANCE:
-			time_related_maintenance_cars.append(car)
+			if time_related_maintenance_cars.has_key(car.brand_name):
+				time_related_maintenance_cars[car.brand_name].append(car)
+			else:
+				time_related_maintenance_cars[car.brand_name] = [car]
 		else:
 			print "error: unknown notify type!"
-
+	output(write_off_cars, distance_related_maintenance_cars, time_related_maintenance_cars)
 
 def get_input():
 	with open('./ThoughtWorks_campus_recruitment_homework.input') as f:
@@ -48,10 +56,29 @@ def get_input():
 def output(write_off_cars, distance_related_maintenance_cars, time_related_maintenance_cars):
 	print 'Reminder'
 	print '=================='
-	print '\n'
+	
 	print '* Time-related maintenance coming soon...'
-	for car in time_related_maintenance_cars:
-		pass
+	for brand_name in sorted(time_related_maintenance_cars.keys()):
+		license_plate_number_list = []
+		for car in time_related_maintenance_cars[brand_name]:
+			license_plate_number_list.append(car.license_plate_number)
+		print "%s: %d (%s)" % (brand_name, len(time_related_maintenance_cars[brand_name]), ", ".join(license_plate_number_list))
+
+	print '\n'
+	print '* Distance-related maintenance coming soon...'
+	for brand_name in sorted(distance_related_maintenance_cars.keys()):
+		license_plate_number_list = []
+		for car in distance_related_maintenance_cars[brand_name]:
+			license_plate_number_list.append(car.license_plate_number)
+		print "%s: %d (%s)" % (brand_name, len(distance_related_maintenance_cars[brand_name]), ", ".join(license_plate_number_list))
+
+	print '\n'
+	print '*Write-off coming soon...'
+	for brand_name in sorted(write_off_cars.keys()):
+		license_plate_number_list = []
+		for car in write_off_cars[brand_name]:
+			license_plate_number_list.append(car.license_plate_number)
+		print "%s: %d (%s)" % (brand_name, len(write_off_cars[brand_name]), ", ".join(license_plate_number_list))
 
 class Car(object):
 	def __init__(self, license_plate_number, purchasing_date, brand_name, running_distance, had_been_maintained):
@@ -68,8 +95,19 @@ class Car(object):
 			print "error input:", had_been_maintained
 
 	def is_notify_time_related_maintenance(self, submit_date):
-
-		return False
+		if self.had_been_maintained:
+			# assume the maintain time is the purchasing time
+			maintenance_interval = 3
+		elif submit_date.year - self.purchasing_date.year >= 3:
+			maintenance_interval = 6
+		else:
+			maintenance_interval = 12
+		month_interval = submit_date.year * 12 + submit_date.month - self.purchasing_date.year * 12 - self.purchasing_date.month
+		# the reason of add 1 is that we need notify it before 1 month
+		if (month_interval + 1) % maintenance_interval == 0:
+			return True
+		else:
+			return False
 
 	def is_notify_distance_related_maintenance(self):
 		return self.running_distance >= 10000 - 500
@@ -79,9 +117,9 @@ class Car(object):
 			time_add = timedelta(days = 365 * 3)
 		else:
 			time_add = timedelta(days = 365 * 6)
-		write_off_date = self.purchasing_date + time_del
+		write_off_date = self.purchasing_date + time_add
 
-		if write_off_date.month > 1:
+		if write_off_date.month == 1:
 			notify_date = date(write_off_date.year - 1, 12, 1)
 		else:
 			notify_date = date(write_off_date.year, write_off_date.month - 1, 1)
